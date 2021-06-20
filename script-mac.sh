35f7c29d056aa85d4f3077e87f4e4e46223c8aee
CrzyBinnic <85854337+CrzyBinnic@users.noreply.github.com>
2021-06-20 21:20:53 +0700

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -1,3 +1,7 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
+
+#Installing rclone
+curl https://rclone.org/install.sh | sudo bash
+
 echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
