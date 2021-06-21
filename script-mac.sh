4a40a2f99d344ec8e4048fd36dda1b09a28d0371
CrzyBinnic <85854337+CrzyBinnic@users.noreply.github.com>
2021-06-22 05:50:14 +0700

    Adding macfuse to script

--- script-mac.sh
+++ script-mac.sh
@@ -4,4 +4,7 @@ sudo mdutil -i off -a
 #Installing rclone
 curl https://rclone.org/install.sh | sudo bash
 
+#Install macfuse for file mounting
+brew install macfuse
+
 echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
